95ccf45d4af4734d0ab07a3699764143ca258cab
Jonathan Gray <jonmattgray@gmail.com>
2022-04-19 12:25:08 +0100

    Use dscp-node types package (#12)
    * use types package
    
    * use configurable api package
    
    * remove logger in buildApi
    
    * use ^ node package

--- app/util/appUtil.js
+++ app/util/appUtil.js
@@ -1,4 +1,4 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api')
+const { buildApi } = require('@digicatapult/dscp-node')
 
 const {
   API_HOST,
@@ -10,78 +10,15 @@ const {
 const logger = require('../logger')
 const { getMemberAliasesDb } = require('../db')
 
-const provider = new WsProvider(`ws://${API_HOST}:${API_PORT}`)
-const apiOptions = {
-  provider,
-  types: {
-    Address: 'MultiAddress',
-    LookupSource: 'MultiAddress',
-    PeerId: 'Vec<u8>',
-    Key: 'Vec<u8>',
-    TokenId: 'u128',
-    RoleKey: 'Role',
-    TokenMetadataKey: `[u8; ${METADATA_KEY_LENGTH}]`,
-    TokenMetadataValue: 'MetadataValue',
-    Token: {
-      id: 'TokenId',
-      original_id: 'TokenId',
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      creator: 'AccountId',
-      created_at: 'BlockNumber',
-      destroyed_at: 'Option<BlockNumber>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parents: 'Vec<TokenId>',
-      children: 'Option<Vec<TokenId>>',
-    },
-    ProcessIO: {
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parent_index: 'Option<u32>',
-    },
-    MetadataValue: {
-      _enum: {
-        File: 'Hash',
-        Literal: `[u8; ${METADATA_VALUE_LITERAL_LENGTH}]`,
-        TokenId: 'TokenId',
-        None: null,
-      },
-    },
-    Role: {
-      _enum: ['Owner', 'Customer', 'AdditiveManufacturer', 'Laboratory', 'Buyer', 'Supplier', 'Reviewer'],
-    },
-    ProcessIdentifier: `[u8; ${PROCESS_IDENTIFIER_LENGTH}]`,
-    ProcessVersion: 'u32',
-    ProcessId: {
-      id: 'ProcessIdentifier',
-      version: 'ProcessVersion',
-    },
-    Process: {
-      status: 'ProcessStatus',
-      restrictions: 'Vec<Restriction>',
-    },
-    ProcessStatus: {
-      _enum: ['Disabled', 'Enabled'],
-    },
-    Restriction: {
-      _enum: {
-        None: '()',
-        SenderOwnsAllInputs: '()',
-        FixedNumberOfInputs: 'FixedNumberOfInputsRestriction',
-        FixedNumberOfOutputs: 'FixedNumberOfOutputsRestriction',
-      },
-    },
-    FixedNumberOfInputsRestriction: {
-      num_inputs: 'u32',
-    },
-    FixedNumberOfOutputsRestriction: {
-      num_outputs: 'u32',
-    },
-    IsNew: 'bool',
-    Restrictions: 'Vec<Restriction>',
+const { api } = buildApi({
+  options: {
+    apiHost: API_HOST,
+    apiPort: API_PORT,
+    metadataKeyLength: METADATA_KEY_LENGTH,
+    metadataValueLiteralLength: METADATA_VALUE_LITERAL_LENGTH,
+    processorIdentifierLength: PROCESS_IDENTIFIER_LENGTH,
   },
-}
-
-const api = new ApiPromise(apiOptions)
+})
 
 api.on('disconnected', () => {
   logger.warn(`Disconnected from substrate node at ${API_HOST}:${API_PORT}`)
