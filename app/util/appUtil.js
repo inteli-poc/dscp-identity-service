edeb87048d6c6311bd3138014d54ba35e0841644
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-07-12 15:55:33 +0100

    Switch to new dscp-node

--- app/util/appUtil.js
+++ app/util/appUtil.js
@@ -1,12 +1,6 @@
 const { buildApi } = require('@digicatapult/dscp-node')
 
-const {
-  API_HOST,
-  API_PORT,
-  METADATA_KEY_LENGTH,
-  METADATA_VALUE_LITERAL_LENGTH,
-  PROCESS_IDENTIFIER_LENGTH,
-} = require('../env')
+const { API_HOST, API_PORT } = require('../env')
 const logger = require('../logger')
 const { getMemberAliasesDb } = require('../db')
 
@@ -14,9 +8,6 @@ const { api } = buildApi({
   options: {
     apiHost: API_HOST,
     apiPort: API_PORT,
-    metadataKeyLength: METADATA_KEY_LENGTH,
-    metadataValueLiteralLength: METADATA_VALUE_LITERAL_LENGTH,
-    processorIdentifierLength: PROCESS_IDENTIFIER_LENGTH,
   },
 })
 
@@ -52,20 +43,7 @@ async function getMembers() {
   return api.query.membership.members()
 }
 
-const utf8ToUint8Array = (str, len) => {
-  const arr = new Uint8Array(len)
-  try {
-    arr.set(Buffer.from(str, 'utf8'))
-  } catch (err) {
-    if (err instanceof RangeError) {
-      throw new Error(`${str} is too long. Max length: ${len} bytes`)
-    } else throw err
-  }
-  return arr
-}
-
 module.exports = {
   getMembers,
   membershipReducer,
-  utf8ToUint8Array,
 }
