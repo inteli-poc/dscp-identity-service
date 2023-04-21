4f61aa7c0c6636f1bf759e8a52a9f41436619a28
Paulius Michelevicius <PMichelevicius@GJ4C3PN73Q.local>
2023-04-21 17:08:41 +0100

    L3-124: linting.

--- app/server.js
+++ app/server.js
@@ -15,7 +15,8 @@ import { verifyJwks } from './util/authUtil.js'
 import promBundle from 'express-prom-bundle'
 import client from 'prom-client'
 
-const { PORT, API_VERSION, API_MAJOR_VERSION, AUTH_TYPE, EXTERNAL_PATH_PREFIX } = env
+const { EXTERNAL_ORIGIN, PORT, API_VERSION, API_MAJOR_VERSION, AUTH_TYPE, EXTERNAL_PATH_PREFIX } = env
+const URL = EXTERNAL_ORIGIN || `http://localhost:${PORT}`
 
 import url from 'url'
 const __filename = url.fileURLToPath(import.meta.url)
@@ -74,18 +75,14 @@ export async function createHttpServer() {
     swaggerOptions: {
       urls: [
         {
-          url: `${v1ApiDoc.servers[0].url}/api-docs`,
+          url: `${URL}/${EXTERNAL_PATH_PREFIX}/${API_MAJOR_VERSION}/api-docs`,
           name: 'IdentityService',
         },
       ],
     },
   }
 
-  app.use(
-    `/${API_MAJOR_VERSION}/swagger`,
-    swaggerUi.serve,
-    swaggerUi.setup(null, options)
-  )
+  app.use(`/${API_MAJOR_VERSION}/swagger`, swaggerUi.serve, swaggerUi.setup(null, options))
 
   // Sorry - app.use checks arity
   // eslint-disable-next-line no-unused-vars
